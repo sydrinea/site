49036bd86be183bb23b2d829080772b7598046c9
Sydney <68250402+cecelot@users.noreply.github.com>
2026-01-25 13:29:48 -0500

    chore: remove old tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { remarkReadingTime } from "./src/readingTime";
 import { baseUrl } from "./data/config.json";
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
@@ -14,7 +13,7 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   output: "static",
   site: baseUrl,
-  integrations: [tailwind(), sitemap(), react(), icon(), mdx()],
+  integrations: [sitemap(), react(), icon(), mdx()],
   markdown: {
     shikiConfig: {
       themes: {
